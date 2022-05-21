6f4ae10691b5d3bcf954ea42f2180b6932c496cf
AlekseiDebrov <105659528+AlekseiDebrov@users.noreply.github.com>
2022-05-21 12:03:34 +0300

    added product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
 $(function(){
 
+
+    $(".ratestar").rateYo({
+        rating: 5,
+        starWidth: "15px"
+        
+      });
+
+      $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow:4,
+        slidesToScroll:4
+      });
+
+
+    var mixer = mixitup('.products__inner-box');
+
+
 });
